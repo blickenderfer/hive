3443de7b576f5896060598de77af277285f61dca
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-08-24 14:28:24 -0400

    added text API call, started navbar, small update to dashboard, updated notes

---
+++ client/src/components/pages/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <div className='container'>
+        <nav>
+            <div className="nav-wrapper">
+                <a href="/dashboard" className="title"><h1>Hive</h1></a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a><Link to="/games">Games</Link></a></li>
+                    <li><a><Link to="/review">Reviews</Link></a></li>
+                    <li><a className="waves-effect waves-light btn-small">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+        </div>
+    )
+}
