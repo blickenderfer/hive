32a489b1c956871ac7e082f34594c59f279ce30a
Pandax19 <ajwhitley@comcast.net>
2023-08-28 18:07:54 -0400

    added review field

--- server/models/Review.js
+++ server/models/Review.js
@@ -12,3 +12,39 @@
 // const Review = model('Review', reviewSchema);
 
 // module.exports = Review
+
+//add component for adding a review, put a fetch request for game names (possibily onClick on a dropdown for the review?) add review button adds review to the server w/the review schema, and post it to user profile?
+//will work on later today brain small
+
+import React, { useState } from 'react';
+
+const ReviewForm = ({ onSubmit }) => {
+    const [rating, setRating] = useState(0);
+    const [comment, setComment] = useState ('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const review = {
+            rating, 
+            comment,
+        };
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Rating:</label>
+                    <input
+                    type="number"
+                    min="1"
+                    max="5"
+                    value={rating}
+                    onChange={(e) => setRating(parseInt(e.target.value))}/>
+                </div>
+                <button type="submit">Submit Review</button>
+            </form>
+        )
+    }
+};
+
+export default ReviewForm;
